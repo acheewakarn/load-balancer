022bedc2e9332d26d1aed88165aa599f54823136
Anon Cheewakarn <acheewakarn@anon-mbp13.attlocal.net>
2023-02-24 00:53:04 -0500

    random load balancer implemented

--- src/load-balancer/main.py
+++ src/load-balancer/main.py
@@ -1,12 +1,26 @@
 import os
 from flask import Flask
+from requests import get
+
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_world(): 
 
-@app.route("/service-info")
-def service_info():
-    return os.environ("SERVICE-PORT")
+    def getRandomServiceAddress():
+        return random.choice(str.split((os.getenv("NODE-ADDRESSES")), ";"))
+
+    service_address = getRandomServiceAddress()
+
+    return get(service_address, verify=False).content
+    # return get("https://example.com", verify=False, timeout=10).content
+
+@app.route("/node-count")
+def node_count():
+    return os.getenv("NODE-COUNT")
+
+@app.route("/node-addresses")
+def node_addresses():
+    return str.split((os.getenv("NODE-ADDRESSES")), ";")
